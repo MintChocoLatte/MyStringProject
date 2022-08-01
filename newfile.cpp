c0c03cc32c7cea180b0cdd3c259ca0e3ff048fb1
MintChocoLatte <applemintchocolatte@apple.com>
2022-08-01 20:07:00 +0900

    assign 기능 추가
    MyString 클래스에
    string_length, memory_capacity 변수 추가
    capacity, reserve, assign 함수 추가
    생성자 함수 변경
    메인 함수 변경

--- newfile.cpp
+++ newfile.cpp
@@ -4,26 +4,35 @@
 class MyString {
 private:
     char* string_content; // 문자열 데이터를 가리키는 포인터
-    int string_length; // 문자열 길이
+    int string_length; // 문자열 길이 
+    int memory_capacity; //현재 할당된 용량
 public:
     MyString(char c);
     MyString(const char* str);
     MyString(const MyString& str);
     ~MyString();
     int length() const;
+    int capacity() const;
+    void reserve(int size);
+    
     void print() const;
     void println() const;  
+    MyString& assign(const MyString& str);
+    MyString& assign(const char* str);
 };
 
 MyString::MyString(char c) 
 {
     string_content = new char[1];
     string_content[0] = c;
+    memory_capacity = 1;
+    string_length = 1;
 }
 
 MyString::MyString(const char* str)
 {
     string_length = strlen(str);
+    memory_capacity = string_length;
     string_content = new char[string_length];
     for(int i = 0; i != string_length; i++)
         string_content[i] = str[i];
@@ -32,6 +41,7 @@ MyString::MyString(const char* str)
 MyString::MyString(const MyString& str)
 {
     string_length = str.string_length;
+    memory_capacity = str.string_length;
     string_content = new char[string_length];
     for(int i = 0; i != string_length; i++)
         string_content[i] = str.string_content[i];
@@ -60,12 +70,66 @@ void MyString::println() const
     std::cout << std::endl;
 }
 
+MyString& MyString::assign(const MyString& str)
+{
+    if(str.string_length > memory_capacity) //기존보다 용량크면
+    {
+        delete[] string_content;    //재할당
+        string_content = new char[str.string_length];
+        memory_capacity = str.string_length;
+    }
+    for(int i = 0; i != str.string_length; i++)
+        string_content[i] = str.string_content[i];
+    string_length = str.string_length;
+    return *this; 
+}
+
+MyString& MyString::assign(const char* str)
+{
+    int str_length = strlen(str);
+    if(str_length > memory_capacity)
+    {
+        delete[] string_content;
+        string_content = new char[str_length];
+        memory_capacity = str_length;
+    }
+    for(int i = 0; i != str_length; i++)
+        string_content[i] = str[i];
+    string_length = str_length;
+    
+    return *this;  
+}
+
+int MyString::capacity() const
+{
+    return memory_capacity;
+}
+
+void MyString::reserve(int size)
+{
+    if(size > memory_capacity)
+    {
+        char* prev_string_content = string_content;
+        string_content = new char[size];
+        memory_capacity = size;
+        for(int i = 0; i != string_length; i++)
+            string_content[i] = prev_string_content[i];
+        delete[] prev_string_content;
+    }
+    //예약하려는 size가 현재 capacity보다 작으면 아무것도 안함
+}    
+
+
+    
+
 int main()
 {
-    MyString str1("Hello world!");
-    MyString str2(str1);
+    MyString str1("very very very long string");
+    str1.reserve(30);
+    
+    std::cout << "Capacity : " << str1.capacity() << std::endl;
+    std::cout << "String length : " << str1.length() << std::endl;
     str1.println();
-    str2.println();
     return 0;
 }
 
